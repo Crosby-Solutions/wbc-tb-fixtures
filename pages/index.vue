cc1d98cf7becf93fff46e3674d48961e6969c054
JCrosby0 <jcrosby@internode.on.net>
2021-10-06 11:38:02 +0800

    testing commits

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,32 @@
 <template>
-<div class="root">
-  <h1>WBC Tee-ball 2021-22 Fixtures</h1>
-  <h2>ToDo List:</h2>
-  <ul>
-    <li>Build content system</li>
-    <li>Input content</li>
-    <li>Build display</li>
-    <li>Hook up content query</li>
-    <li>Build Filter UI</li>
-    <li>Hook filters into the query</li>
-    <li>Persist filters to local storage</li>
-    <li>Deploy to Nelify</li>
-  </ul>
-  <h2>
-    Stretch functionality - TBD
-  </h2>
-  <ul>
-    <li>Team Pages</li>
-    <li>Manager contact info</li>
-    <li>Venue maps</li>
-    <li>Diamond Overlays</li>
-  </ul>
-</div>
+  <div class="root">
+    <h1 class="text-xl">
+      WBC Tee-ball 2021-22 Fixtures
+    </h1>
+    <h2 class="text-lg pt-4">
+      ToDo List:
+    </h2>
+    <ul>
+      <li>[x] Build content system</li>
+      <li>[x] Deploy to Nelify</li>
+      <li>[ ] Set up access to admin</li>
+      <li>[ ] Input content</li>
+      <li>[ ] Build display</li>
+      <li>[ ] Hook up content query</li>
+      <li>[ ] Build Filter UI</li>
+      <li>[ ] Hook filters into the query</li>
+      <li>[ ] Persist filters to local storage</li>
+    </ul>
+    <h2 class="text-lg pt-4">
+      Stretch functionality - TBD
+    </h2>
+    <ul>
+      <li>[ ] Team Pages</li>
+      <li>[ ] Manager contact info</li>
+      <li>[ ] Venue maps</li>
+      <li>[ ] Diamond Overlays</li>
+    </ul>
+  </div>
 </template>
 
 <script>
