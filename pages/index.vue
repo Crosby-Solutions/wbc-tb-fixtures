e12875e1398ff57f591968b36c009e4747503373
JCrosby0 <jcrosby@internode.on.net>
2021-10-06 11:20:16 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="root">
+  <h1>WBC Tee-ball 2021-22 Fixtures</h1>
+  <h2>ToDo List:</h2>
+  <ul>
+    <li>Build content system</li>
+    <li>Input content</li>
+    <li>Build display</li>
+    <li>Hook up content query</li>
+    <li>Build Filter UI</li>
+    <li>Hook filters into the query</li>
+    <li>Persist filters to local storage</li>
+    <li>Deploy to Nelify</li>
+  </ul>
+  <h2>
+    Stretch functionality - TBD
+  </h2>
+  <ul>
+    <li>Team Pages</li>
+    <li>Manager contact info</li>
+    <li>Venue maps</li>
+    <li>Diamond Overlays</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
